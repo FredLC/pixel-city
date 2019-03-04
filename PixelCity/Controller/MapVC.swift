a913ee5f25d141b16f29d697f23a4aaf82d3a16e
fredlc <fredlefevre@outlook.com>
2019-03-04 15:06:48 -0500

    Initial project setup

---
+++ PixelCity/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PixelCity
+//
+//  Created by Fred Lefevre on 2019-03-04.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
